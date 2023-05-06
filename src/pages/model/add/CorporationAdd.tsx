b0fd6f40a7b0a9ca035837f152a8f421285404a5
xxmehmetali <xxmehmetali@hotmail.com>
2023-05-06 10:54:08 +0300

    Custom date picker added and can be used with formik

--- src/pages/model/add/CorporationAdd.tsx
+++ src/pages/model/add/CorporationAdd.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import CorporationAddForm from '../../../components/addcomponents/CorporationAddComponent';
 import { Formik } from 'formik';
 import { corporationInitialValue } from '../../../yup_schemas/initialValues/corporationInitialValue';
@@ -8,56 +8,47 @@ import { Button, Form } from 'react-bootstrap';
 import CustomInput from '../../../components/customFormElements/CustomInput';
 import { useAddCorporationMutation } from '../../../features/api/corporationApi';
 
-export default function CorporationAdd() {
-  
-  const [addCorporation, {isLoading}] = useAddCorporationMutation();
-  function onSubmit(values: any, actions: any){
+import CustomDatePicker from '../../../components/customFormElements/CustomDatePicker';
+import { Calendar } from 'primereact/calendar';
 
-      console.log("xxx")
-      console.log(values)
-  
-      addCorporation(values)
+export default function CorporationAdd() {
+  const [addCorporation, { isLoading }] = useAddCorporationMutation();
+  function onSubmit(values: any, actions: any) {
+    addCorporation(values)
     actions.resetForm();
-  
-    /*
-    name: string;
-    description: string;
-    taxNumber: string;
-    foundationDate: Date;
-    */
   }
 
   return (
     <div>
       {/* <CorporationAddForm/> */}
-
+      {/* <Calendar value={dateTime24h} onChange={(e) => setDateTime24h(e.value as Date)} showTime hourFormat="24" /> */}
       <Formik
-      initialValues={corporationInitialValue}
-      validationSchema={yup.object({
-        name: yup.string().required("Name required!").min(3, "Name is too short!"),
-        description: yup.string().required("Description required!").min(6, "Description is too short!"),
-        taxNumber: yup.string().required("Tax Number Required!").min(5).max(14),
-        foundationDate : yup.date().required()
-      })}
-      onSubmit={onSubmit}
-    >
-      {formik => (
-        <Form
-          onSubmit={formik.handleSubmit}
-        >
-         <CustomInput name="name" placeholder="Enter Name" label={"Name"}/>
-         <CustomInput name="description" placeholder="Enter Description" type="text" label={"Description"}/>
-         <CustomInput name="taxNumber" placeholder="Enter Tax Number" type="text" label={"Tax Number"}/>
-         <CustomInput name="foundationDate" placeholder="Enter Foundation Date" type="text" label={"Foundation Date"}/>
-         {/* <CustomInput name="projects" placeholder="Enter id" label={"proj"}/> */}
+        initialValues={corporationInitialValue}
+        validationSchema={yup.object({
+          name: yup.string().required("Name required!").min(3, "Name is too short!"),
+          description: yup.string().required("Description required!").min(6, "Description is too short!"),
+          taxNumber: yup.string().required("Tax Number Required!").min(5).max(14),
+          foundationDate: yup.date().required("No Date is entered!")
+        })}
+        onSubmit={onSubmit}
+      >
+        {formik => (
+          <Form
+            onSubmit={formik.handleSubmit}
+          >
+            <CustomInput name="name" placeholder="Enter Name" label={"Name"} />
+            <CustomInput name="description" placeholder="Enter Description" type="text" label={"Description"} />
+            <CustomInput name="taxNumber" placeholder="Enter Tax Number" type="text" label={"Tax Number"} />
+            {/* <CustomInput name="foundationDate" placeholder="Enter Foundation Date" type="text" label={"Foundation Date"}/> */}
+            <CustomDatePicker name="foundationDate" label={"Foundation Date"} placeholder="Please provide a Date" />
 
-          <Button type="submit">
-            Add Corporation
-          </Button>
-        </Form>
-      )}
-    </Formik>
+
+            <Button type="submit" style={{marginTop:"1em"}}>
+              Add Corporation
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
-    
